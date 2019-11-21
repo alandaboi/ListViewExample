eb9db7f88b6f75055bef326c357eb49d6a72d635
Alan <alanxiao2013@gmail.com>
2019-11-21 08:41:57 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/listviewexample/MainActivity.java
+++ app/src/main/java/com/example/listviewexample/MainActivity.java
@@ -54,7 +54,7 @@ public class MainActivity extends AppCompatActivity  {
         String sport[] = new String[] {"Golf", "Tennis", "Badminton", "Soccer", "Football", "Basketball", "Gaming", "Hockey", "Swimming", "Track", "Cross Country", "Gymnastics", "Ice Hockey", "Figure Skating", "Volleyball"};
         String name[] = new String[] {"Mr. Miro", "Alan", "David", "Alex", "Jimmy", "Sunny", "Jesse", "Ronal", "Colin", "Jarvis", "Cindy", "Cathy", "Hedwig", "Quinn", "Philip"};
         String webpage[] = new String[] {"mirojurisic", "alandaboi", "firestix475", "LexingtonCV16-san", "JimmyRaven", "sunnnnny-cyber", "mgrddsj", "RonalYu", "ColinLiangWJ", "niceTimnice", "Cindy4869", "Cathy1399485071", "ASIA11793", "ZhangRunHui", "pilip11674"};
-        int imageResource[] = new int[] {R.drawable.a, R.drawable.b, R.drawable.c, R.drawable.d, R.drawable.e, R.drawable.f, R.drawable.g, R.drawable.h, R.drawable.i, R.drawable.j, R.drawable.k, R.drawable.l, R.drawable.m, R.drawable.n, R.drawable.o};
+        int imageResource[] = new int[] {R.drawable.miro, R.drawable.alan, R.drawable.david, R.drawable.alex, R.drawable.jimmy, R.drawable.sunny, R.drawable.jesse, R.drawable.ronal, R.drawable.colin, R.drawable.jarvis, R.drawable.cindy, R.drawable.cathy, R.drawable.hedwig, R.drawable.quinn, R.drawable.philip};
         int worth[] = new int[] {15, 14, 13, 12, 11, 10, 9, 8, 7, 60, 5, 4, 3, 2, 1};
         int age[] = new int[] {50, 49, 48, 47, 46, 45, 44, 43, 42, 20, 40, 39, 38, 37, 36};
         list   = new ArrayList<Player>();
