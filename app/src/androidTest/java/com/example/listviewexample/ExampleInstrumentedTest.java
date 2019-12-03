fa23e9b2dfefd062cd1b0233d7be92c06f1bb383
Alan <alanxiao2013@gmail.com>
2019-12-03 08:40:31 +0800

    Mr. Miro's updates

--- app/src/androidTest/java/com/example/listviewexample/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/listviewexample/ExampleInstrumentedTest.java
@@ -13,7 +13,7 @@ import static org.junit.Assert.*;
 /**
  * Instrumented test, which will execute on an Android device.
  *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * @see <miro href="http://d.android.com/tools/testing">Testing documentation</miro>
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
